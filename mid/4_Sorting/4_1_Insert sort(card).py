910ce2ffe454b6ed195c2f7f2427e3b4157420cd
[nanana] <[100232@naver.com]>
2025-04-21 09:38:56 +0900

    250421

--- mid/4_Sorting/4_1_Insert sort(card).py
+++ mid/4_Sorting/4_1_Insert sort(card).py
@@ -1,1 +1,13 @@
 
+def insertion_sort(data):
+    for j in range(1,len(data)):
+        key = data[j]
+        i = j-1
+        while i>=0 and data[i] > key:
+            data[i+1] = data[i]
+            i -= 1
+        data[i+1] = key
+
+test =[5,2,4,6,1,3]
+insertion_sort(test)
+print(test)
